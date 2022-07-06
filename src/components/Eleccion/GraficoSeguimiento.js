25819b656a559fc7e8026291db8ed69b2538ff3b
Eduardo Diturbide <eduardo.diturbide@iosper.gov.ar>
2022-07-06 11:05:25 -0300

    mesas escrutadas

--- src/components/Eleccion/GraficoSeguimiento.js
+++ src/components/Eleccion/GraficoSeguimiento.js
@@ -1,16 +1,25 @@
-import { Box, Container, Grid, LinearProgress } from '@mui/material';
+import { Box, Card, CardContent, Grid, LinearProgress, List, ListItem, ListItemText, Typography } from '@mui/material';
 import React from 'react'
 import { Chart } from "react-google-charts";
 import { getResultadosEleccion } from '../../services/Eleccion/Eleccion';
 
-const getInfo = listas => {
-    const dataG = [
-        ["Lista", "Votos"],
-    ];
+const options = {
+    chartArea: {
+        width:'100%',
+        height: '80%',
+    },
+    legend: { 
+      position: 'left', 
+      alignment: 'center',
+      textStyle: { 
+        fontSize: '15' 
+      } 
+    }
+};
 
-    listas?.map((i) => 
-        dataG.push([i.nombre, i.cantidadvoto])
-    )
+const getInfo = listas => {
+    const dataG = [["Lista", "Votos"],];
+    listas?.map((i) => dataG.push([i.nombre , i.cantidadvoto]))
 
     return dataG;
 }
@@ -19,46 +28,127 @@ const GraficoSeguimiento = () => {
     const [data, setData] = React.useState(null);
     
     React.useEffect(() => {
-        const getData = async () => {
+        const getDataResultados = async () => {
             const res = await getResultadosEleccion();
             setData(res?.data);     
         };
 
         const interval = setInterval(() => {
-            getData();
+            getDataResultados();
         }, 10000);
 
         return () => clearInterval(interval);
 
     }, [])
     
-
-
     return (
         <>
             {!data && (
-                <Grid
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    
-                        <LinearProgress sx={{ mt: '30px'}}/>
-                    
+                <Grid container sx={{ display: 'flex', alignItems: 'flex-end', height: '85vh' }}>
+                    <Box sx={{ width: '100%' }}>
+                        <LinearProgress />
+                    </Box>
                 </Grid>
             )}
             {data && (
                 <Grid container spacing={2} sx={{ height: '100%' }}>
-                    {data?.map((item, key) => (
-                        <Grid item md={6} key={key}>
-                            {item.agrupamiento}
-                            <Chart
-                                chartType="PieChart"
-                                data={getInfo(item.listas)}
-                                width="100%"
-                                height="400px"
-                                legendToggle
-                            />
+                    {data?.mesas_escrutadas && (
+                        <Grid item xs={12}>
+                            <Card variant="outlined">
+                                <CardContent>
+                                    <Grid container>
+                                        <Grid item xs={12} md={4}>
+                                            <List>
+                                                <ListItem sx={{ py: 0 }}>
+                                                    <ListItemText 
+                                                        primary="Mesas habilitadas"
+                                                        secondary={
+                                                            <Typography variant="h5" component="div" sx={{ 
+                                                                color: '#6c757d', 
+                                                                fontSize: '1.5rem',
+                                                                fontWeight: '300'
+                                                            }}>
+                                                                {data?.mesas_habilitadas}
+                                                            </Typography>
+                                                        } />
+                                                </ListItem>
+                                            </List>
+                                        </Grid>     
+                                        <Grid item xs={12} md={4}>
+                                            <List>
+                                                <ListItem sx={{ py: 0 }}>
+                                                    <ListItemText 
+                                                        primary="Mesas escrutadas"
+                                                        secondary={
+                                                            <Typography variant="h5" component="div" sx={{ 
+                                                                color: '#6c757d', 
+                                                                fontSize: '1.5rem',
+                                                                fontWeight: '300'
+                                                            }}>
+                                                                {data?.mesas_escrutadas}
+                                                            </Typography>
+                                                        } />
+                                                </ListItem>
+                                            </List>
+                                        </Grid>     
+                                        <Grid item xs={12} md={4}>
+                                            <List>
+                                                <ListItem sx={{ py: 0 }}>
+                                                    <ListItemText 
+                                                        primary="Porcentaje"
+                                                        secondary={
+                                                            <Typography variant="h5" component="div" sx={{ 
+                                                                color: '#6c757d', 
+                                                                fontSize: '1.5rem',
+                                                                fontWeight: '300'
+                                                            }}>
+                                                                {data?.mesas_habilitadas*(data?.mesas_escrutadas/100)}%
+                                                            </Typography>
+                                                        } />
+                                                </ListItem>
+                                            </List>
+                                        </Grid>     
+                                    </Grid>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    )}
+
+                    {data?.resultados?.map((item, key) => (
+                        <Grid item xs={12} md={6} key={key}>
+                            <Card variant="outlined">
+                                <CardContent>
+                                    <Typography variant="h5" component="div">
+                                        {item.agrupamiento}
+                                    </Typography>
+                                    <Chart
+                                        chartType="PieChart"
+                                        options={options}
+                                        data={getInfo(item.listas)}
+                                        width="100%"
+                                        height="300px"
+                                        legendToggle
+                                    />
+                                    <Grid container>
+                                        <List>
+                                            {item?.listas?.map((i, k) => (
+                                                <ListItem key={k} sx={{ py: 0 }}>
+                                                    <ListItemText primary={`${i.nombre}`} 
+                                                        secondary={
+                                                            <Typography variant="h5" component="div" sx={{ 
+                                                                color: '#6c757d', 
+                                                                fontSize: '1.5rem',
+                                                                fontWeight: '300'
+                                                            }}>
+                                                                {i.cantidadvoto} votos
+                                                            </Typography>
+                                                        } />
+                                                </ListItem>
+                                            ))}
+                                        </List>
+                                    </Grid>
+                                </CardContent>
+                            </Card>
                         </Grid>
                     ))}
                 </Grid>
